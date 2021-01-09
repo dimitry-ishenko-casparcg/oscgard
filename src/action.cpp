df498f8aa8347eb6f6f98efb8412e926cd1e7026
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2021-01-09 15:01:38 -0500

    action: tweaks

--- src/action.cpp
+++ src/action.cpp
@@ -33,6 +33,7 @@ auto to_string(const osc::value& v)
          if(v.is_int32 ()) s = std::to_string(v.to_int32 ());
     else if(v.is_float ()) s = std::to_string(v.to_float ());
     else if(v.is_string()) s = v.to_string();
+    else if(v.is_blob  ()) s = ""; // TODO: convert to hex
     else if(v.is_int64 ()) s = std::to_string(v.to_int64 ());
     else if(v.is_time  ()) s = '@' + std::to_string(v.to_time().time_since_epoch().count());
     else if(v.is_double()) s = std::to_string(v.to_double());
@@ -45,14 +46,31 @@ auto to_string(const osc::value& v)
     return s;
 }
 
-void replace(std::string& s, const std::string& from, const std::string& to)
+void replace_token(std::string& s, const std::string& tkn, const std::string& to)
 {
     std::size_t p = 0;
-    while((p = s.find(from, p)) != std::string::npos)
+    while((p = s.find(tkn, p)) != std::string::npos)
     {
-        s.replace(p, from.size(), to);
+        s.replace(p, tkn.size(), to);
         p += to.size();
     }
+}
+
+auto replace_tokens(const std::vector<std::string>& args, const osc::message& msg)
+{
+    auto args_ { args };
+
+    for(auto& arg : args_) replace_token(arg, "{A}", msg.address());
+
+    int n = 0;
+    for(auto const& val : msg.values())
+    {
+        auto tkn = '{' + std::to_string(n++) + '}';
+        auto to = to_string(val);
+        for(auto& arg : args_) replace_token(arg, tkn, to);
+    }
+
+    return args_;
 }
 
 }
@@ -60,20 +78,10 @@ void replace(std::string& s, const std::string& from, const std::string& to)
 ////////////////////////////////////////////////////////////////////////////////
 void action::operator()(const osc::message& msg)
 {
-    auto args_2 = args;
-
-    for(auto& arg : args_2) replace(arg, "{A}", msg.address());
-
-    int n = 0;
-    for(auto const& val : msg.values())
-    {
-        auto from = '{' + std::to_string(n++) + '}';
-        auto to = to_string(val);
-        for(auto& arg : args_2) replace(arg, from, to);
-    }
+    auto args_ = replace_tokens(args, msg);
 
     if(auto pid = fork(); pid == child)
-        exec(file, args_2);
+        exec(file, args_);
     else std::cout << "Starting " << (*this) << " as process " << pid << std::endl;
 }
 
