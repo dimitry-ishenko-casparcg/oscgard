1af745030680112bc34c2035da74e3d2d4db33ee
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2021-01-19 17:45:49 -0500

    Replace args with pgm::args

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,18 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 #include "actions.hpp"
-#include "args.hpp"
+#include "pgm/args.hpp"
 #include "server.hpp"
 #include "system.hpp"
 
 #include <asio.hpp>
-#include <exception>
+#include <cstdint>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
-using namespace asio::ip;
 namespace fs = std::filesystem;
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -28,25 +29,31 @@ namespace fs = std::filesystem;
 #define TEXT_(x) #x
 
 ////////////////////////////////////////////////////////////////////////////////
-std::string usage(const std::string& name)
+std::string version(const std::string& name)
 {
-    return "Usage: " + name + R"( [option] [path]
-
-Where [path] is an optional path to an alternative actions file.
-
-Option is one or more of the following:
-
-    --address=<addr>    Specify IP address to bind to. Default: bind to all
-                        addresses.
-
-    --help, -h          Print this help screen and exit.
-
-    --port=<n>          Specify port to listen on. Default: 6260.
-
-    --version, -v       Show version and exit.)";
+    return name + " version " TEXT(VERSION);
 }
 
-std::string version(const std::string& name) { return name + " version " TEXT(VERSION); }
+////////////////////////////////////////////////////////////////////////////////
+auto to_address(const std::string& s)
+{
+    asio::error_code ec;
+    auto address = asio::ip::make_address(s, ec);
+
+    if(!ec) return address;
+    else throw pgm::invalid_argument{ "Invalid IP address", s };
+}
+
+////////////////////////////////////////////////////////////////////////////////
+auto to_port(const std::string& s)
+{
+    char* end;
+    auto ul = std::strtoul(s.data(), &end, 0);
+
+    if(ul <= UINT16_MAX && end == (s.data() + s.size()))
+        return static_cast<std::uint16_t>(ul);
+    else throw pgm::invalid_argument{ "Invalid port number", s };
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[])
@@ -54,47 +61,54 @@ int main(int argc, char* argv[])
     int exit_code = 0;
     try
     {
-        auto name = fs::path(argv[0]).filename();
+        auto name{ fs::path(argv[0]).filename() };
+        pgm::args args
+        {
+            { "-a", "--address", "addr", "Specify IP address to bind to. Default: 0.0.0.0\n"
+                                         "(bind to all addresses)."             },
+            { "-p", "--port", "N",       "Specify port number to listen on Default: 6260." },
+            { "-h", "--help",            "Print this help screen and exit."     },
+            { "-v", "--version",         "Show version number and exit."        },
+            { "actions?",                "Path to alternate actions file."      }
+        };
+        args.parse(argc, argv);
 
-        auto args = src::args::read_from(argc, argv);
-        if(args.help)
-        {
-            std::cout << usage(name) << std::endl;
-        }
-        else if(args.version)
-        {
+        if(args["--help"])
+            std::cout << args.usage(name) << std::endl;
+
+        else if(args["--version"])
             std::cout << version(name) << std::endl;
-        }
+
         else
         {
-            if(args.path.empty())
+            fs::path path{ args["actions"].value_or("") };
+            if(path.empty())
             {
-                args.path = src::data_path() / name / "actions.conf";
+                path = src::data_path() / name / "actions.conf";
 
-                fs::create_directory(args.path.parent_path());
-                if(!fs::exists(args.path)) std::fstream{ args.path, std::ios::out };
+                fs::create_directory(path.parent_path());
+                if(!fs::exists(path)) std::fstream{ path, std::ios::out };
             }
 
-            std::cout << "Reading actions from " << args.path << std::endl;
-            auto acts = src::actions::read_from(args.path);
+            std::cout << "Reading actions from " << path << "." << std::endl;
+            auto acts{ src::actions::read_from(path) };
 
-            if(args.port == 0) args.port = 6260;
+            auto address{ to_address(args["--address"].value_or("0.0.0.0")) };
+            auto port{ to_port(args["--port"].value_or("6260")) };
 
-            auto local = args.address.is_unspecified()
-                ? udp::endpoint(udp::v4(), args.port)
-                : udp::endpoint(args.address, args.port);
-            std::cout << "Listening on " << local << std::endl;
+            asio::ip::udp::endpoint local{ address, port };
+            std::cout << "Listening on " << local << "." << std::endl;
 
             asio::io_context io;
             src::server server(io, local, acts);
 
             src::set_interrupt_callback([&](int signal)
             {
-                std::cout << "Received signal " << signal << " - exiting" << std::endl;
+                std::cout << "Received signal " << signal << " - exiting." << std::endl;
                 io.stop();
             });
 
-            std::cout << "Starting event loop" << std::endl;
+            std::cout << "Starting event loop." << std::endl;
             io.run();
         }
     }
