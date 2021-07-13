bbfa508077486fe3ef326aa3dd06c8537a2d791e
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2021-07-12 21:04:31 -0400

    main: simplify

--- src/main.cpp
+++ src/main.cpp
@@ -20,16 +20,9 @@
 
 namespace fs = std::filesystem;
 
-////////////////////////////////////////////////////////////////////////////////
 #if !defined(VERSION)
 #  define VERSION "0"
 #endif
-
-////////////////////////////////////////////////////////////////////////////////
-std::string version(const std::string& name)
-{
-    return name + " version " + VERSION;
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 auto to_address(const std::string& s)
@@ -54,70 +47,68 @@ auto to_port(const std::string& s)
 
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[])
+try
 {
-    int exit_code = 0;
-    try
+    auto name{ fs::path(argv[0]).filename() };
+    pgm::args args
     {
-        auto name{ fs::path(argv[0]).filename() };
-        pgm::args args
+        { "-a", "--address", "addr", "Specify IP address to bind to. Default: 0.0.0.0\n"
+                                     "(bind to all addresses)."         },
+        { "-p", "--port", "N",       "Specify port number to listen on Default: 6260." },
+        { "-h", "--help",            "Print this help screen and exit." },
+        { "-v", "--version",         "Show version number and exit."    },
+        { "actions?",                "Path to alternate actions file."  },
+    };
+    args.parse(argc, argv);
+
+    if(args["--help"])
+        std::cout << args.usage(name) << std::endl;
+
+    else if(args["--version"])
+        std::cout << name.string() << " version " << VERSION << std::endl;
+
+    else
+    {
+        fs::path path{ args["actions"].value_or("") };
+        if(path.empty())
         {
-            { "-a", "--address", "addr", "Specify IP address to bind to. Default: 0.0.0.0\n"
-                                         "(bind to all addresses)."             },
-            { "-p", "--port", "N",       "Specify port number to listen on Default: 6260." },
-            { "-h", "--help",            "Print this help screen and exit."     },
-            { "-v", "--version",         "Show version number and exit."        },
-            { "actions?",                "Path to alternate actions file."      }
-        };
-        args.parse(argc, argv);
+            path = src::data_path() / name / "actions.conf";
 
-        if(args["--help"])
-            std::cout << args.usage(name) << std::endl;
-
-        else if(args["--version"])
-            std::cout << version(name) << std::endl;
-
-        else
-        {
-            fs::path path{ args["actions"].value_or("") };
-            if(path.empty())
-            {
-                path = src::data_path() / name / "actions.conf";
-
-                fs::create_directory(path.parent_path());
-                if(!fs::exists(path)) std::fstream{ path, std::ios::out };
-            }
-
-            std::cout << "Reading actions from " << path << "." << std::endl;
-            auto acts = src::read_actions(path);
-
-            auto address{ to_address(args["--address"].value_or("0.0.0.0")) };
-            auto port{ to_port(args["--port"].value_or("6260")) };
-
-            asio::ip::udp::endpoint local{ address, port };
-            std::cout << "Listening on " << local << "." << std::endl;
-
-            asio::io_context io;
-            src::server server{ io, local, acts };
-
-            src::set_interrupt_callback([&](int signal)
-            {
-                std::cout << "Received signal " << signal << " - exiting." << std::endl;
-                io.stop();
-            });
-
-            std::cout << "Starting event loop." << std::endl;
-            io.run();
+            fs::create_directory(path.parent_path());
+            if(!fs::exists(path)) std::fstream{ path, std::ios::out };
         }
+
+        std::cout << "Reading actions from " << path << "." << std::endl;
+        auto acts = src::read_actions(path);
+
+        auto address{ to_address(args["--address"].value_or("0.0.0.0")) };
+        auto port{ to_port(args["--port"].value_or("6260")) };
+
+        asio::ip::udp::endpoint local{ address, port };
+        std::cout << "Listening on " << local << "." << std::endl;
+
+        asio::io_context io;
+        src::server server{ io, local, acts };
+
+        src::set_interrupt_callback([&](int signal)
+        {
+            std::cout << "Received signal " << signal << " - exiting." << std::endl;
+            io.stop();
+        });
+
+        std::cout << "Starting event loop." << std::endl;
+        io.run();
+
+        return 0;
     }
-    catch(std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-        exit_code = 1;
-    }
-    catch(...)
-    {
-        std::cerr << "???" << std::endl;
-        exit_code = 1;
-    }
-    return exit_code;
+}
+catch(std::exception& e)
+{
+    std::cerr << e.what() << std::endl;
+    return 1;
+}
+catch(...)
+{
+    std::cerr << "???" << std::endl;
+    return 1;
 }
