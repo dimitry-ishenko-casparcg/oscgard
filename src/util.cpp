148da000e97e7df76291a3b0301ed04e0afc6977
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2021-07-17 16:31:14 -0400

    util: update copyright year

--- src/util.cpp
+++ src/util.cpp
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2020 Dimitry Ishenko
+// Copyright (c) 2020-2021 Dimitry Ishenko
 // Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
 //
 // Distributed under the GNU GPL license. See the LICENSE.md file for details.
